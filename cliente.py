0267d94b613fe13b4fe4ad262e0ab767afd885a6
Nathan <nathansaraujo191@gmail.com>
2022-12-01 10:23:29 -0300

    adicionando arquivos

---
+++ cliente.py
@@ -0,0 +1,34 @@
+# Implementação baseada no material do campus e utilizando como base o 
+# video: https://www.youtube.com/watch?v=slC-qNFm6hg
+
+import socket
+import threading
+
+apelido = input("Escolha um apelido: ")
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect(('127.0.0.1', 55555))
+
+def receive():
+    while True:
+        try:
+            mensagem = cliente.recv(1024).decode('ascii')
+            if mensagem == 'NOME':
+                cliente.send(apelido.encode('ascii'))
+            else:
+                print(mensagem)
+        except:
+            print('Um erro ocorreu!')
+            cliente.close()
+            break
+
+def write():
+    while True:
+        mensagem = f'{apelido}: {input("")}'
+        cliente.send(mensagem.encode('ascii'))
+
+receiveThread = threading.Thread(target=receive)
+receiveThread.start()
+
+writeThread = threading.Thread(target=write)
+writeThread.start()
---
+++ servidor.py
@@ -0,0 +1,53 @@
+# Implementação baseada no material do campus e utilizando como base o 
+# video: https://www.youtube.com/watch?v=slC-qNFm6hg
+
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 55555
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((host, port))
+servidor.listen()
+
+clientes = []
+nicknames = []
+
+def broadcast(message):
+    for cliente in clientes:
+        cliente.send(message)
+
+def handle(cliente):
+    while True:
+        try:
+            message = cliente.recv(1024)
+            broadcast(message)
+        except:
+            index = clientes.index(cliente)
+            clientes.remove(cliente)
+            cliente.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} saiu do chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        cliente, address = servidor.accept()
+        print(f'Conectado com {str(address)}')
+
+        cliente.send('NOME'.encode('ascii'))
+        nickname = cliente.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clientes.append(cliente)
+
+        print(f'O nome do clientee eh {nickname}!')
+        broadcast(f'{nickname} entrou no chat!'.encode('ascii'))
+        cliente.send('Conectado no servidor!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(cliente,))
+        thread.start()
+
+print('Servidor está online...')
+receive()
